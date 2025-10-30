e1724ff8b1f06cfbbbefe85a7f7453b4b556247c
ldirocco <lorenzo.dirocco@uniroma1.it>
2025-10-30 14:06:50 +0100

    cli updating

--- hyperbiome/trainer.py
+++ hyperbiome/trainer.py
@@ -21,7 +21,7 @@ def train_multiproxy_model(model, dataloader, optimizer, proxy_loss_fn, device):
         x, y_species, y_genus = x.to(device), y_species.to(device), y_genus.to(device)
         optimizer.zero_grad()
         emb= model(x)
-        loss = proxy_loss_fn(emb, y_species, y_genus)
+        loss = proxy_loss_fn(emb, y_species)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
